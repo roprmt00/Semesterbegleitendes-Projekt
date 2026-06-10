01feb47471d07f047febe06a3a9fe5165c2d1198
Mert Gueclue <megumt00@hs-esslingen.de>
2026-06-11 01:32:52 +0200

    Includes und main eingebunden

--- rechner.cpp
+++ rechner.cpp
@@ -1,3 +1,11 @@
-# Berechnungen
+// Berechnungen
 
-In diesem File werden die Berechnungen mithilfe den Formeln aus der Formel.md Datei durchgeführt und dargestellt.
+//In diesem File werden die Berechnungen mithilfe den Formeln aus der Formel.md Datei durchgeführt und dargestellt.
+
+#include <iostream> //Hier binde Ich die Standardbibliotheken ein für Ein und Augabe
+#include <cmath> //Hier binde Ich die cmath Bibliothek ein, damit Ich die pow Funktion verwenden kann
+
+int main (){
+
+}
+
