99545681ecc8d4535b2bb011b251b8bfb30c2b7d
Robin Probst <robin.probst@hs-esslingen.de>
2026-06-23 20:45:48 +0200

    Strukturnamen zu Englisch geändert, Operator "==" hinzugefügt

--- include/readJson.h
+++ include/readJson.h
@@ -8,16 +8,24 @@
 
 
 
-struct WetterDaten {
+struct WeatherData {
     bool Regen;
     double Temp;
     double Windgeschw;
     bool Gegenwind;
     bool Rueckenwind;
+
+    bool operator==(const WeatherData& other) const {
+        return Regen == other.Regen &&
+               Temp == other.Temp &&
+               Windgeschw == other.Windgeschw &&
+               Gegenwind == other.Gegenwind &&
+               Rueckenwind == other.Rueckenwind;
+    }
 };
 
 
-struct Streckenprofil {
+struct RouteData {
     std::string Name;
     double Distanz;
     double Distanz_Land;
@@ -28,18 +36,37 @@ struct Streckenprofil {
     double Durchschnitt_Autobahn;
     double Hoehenmeter_bergauf;
     double Hoehenmeter_bergab;
+
+    bool operator==(const RouteData& other) const {
+        return Name == other.Name &&
+               Distanz == other.Distanz &&
+               Distanz_Land == other.Distanz_Land &&
+               Distanz_Stadt == other.Distanz_Stadt &&
+               Distanz_Autobahn == other.Distanz_Autobahn &&
+               Durchschnitt_Land == other.Durchschnitt_Land &&
+               Durchschnitt_Stadt == other.Durchschnitt_Stadt &&
+               Durchschnitt_Autobahn == other.Durchschnitt_Autobahn &&
+               Hoehenmeter_bergauf == other.Hoehenmeter_bergauf &&
+               Hoehenmeter_bergab == other.Hoehenmeter_bergab;
+    }
 };
 
 
-struct Fahrzeugparameter {
+struct CarData {
     std::string Name;
     double Batteriekapazitaet;
     double Durchschn_Verbrauch;
+
+    bool operator==(const CarData& other) const {
+        return Name == other.Name &&
+               Batteriekapazitaet == other.Batteriekapazitaet &&
+               Durchschn_Verbrauch == other.Durchschn_Verbrauch;
+    }
 };
 
 
-std::optional<WetterDaten> getWeatherData(const std::filesystem::path& filepath);
-std::optional<Streckenprofil> getRouteData(const std::filesystem::path& filepath);
-std::optional<Fahrzeugparameter> getCarData(const std::filesystem::path& filepath);
+std::optional<WeatherData> getWeatherData(const std::filesystem::path& filepath);
+std::optional<RouteData> getRouteData(const std::filesystem::path& filepath);
+std::optional<CarData> getCarData(const std::filesystem::path& filepath);
 
 #endif 
